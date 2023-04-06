f2bccbfcdabeb34e58dba2079bf0ac39fd65206d
pnadolny13 <patnadolny@gmail.com>
2023-04-06 19:30:27 -0400

    add dynamodb wrapper and test it using moto

---
+++ tap_dynamodb/dynamo.py
@@ -0,0 +1,82 @@
+import logging
+import os
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+class DynamoDB:
+    def __init__(self):
+        self._client = None
+        self.logger = logging.getLogger(__name__)
+
+    def authenticate(self, config):
+        if self._client:
+            return
+
+        aws_access_key_id = config.get("aws_access_key_id") or os.environ.get(
+            "AWS_ACCESS_KEY_ID"
+        )
+        aws_secret_access_key = config.get("aws_secret_access_key") or os.environ.get(
+            "AWS_SECRET_ACCESS_KEY"
+        )
+        aws_session_token = config.get("aws_session_token") or os.environ.get(
+            "AWS_SESSION_TOKEN"
+        )
+        aws_profile = config.get("aws_profile") or os.environ.get("AWS_PROFILE")
+        aws_endpoint_url = config.get("aws_endpoint_url")
+        aws_region_name = config.get("aws_region_name")
+
+        # AWS credentials based authentication
+        if aws_access_key_id and aws_secret_access_key:
+            aws_session = boto3.session.Session(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=aws_session_token,
+                region_name=aws_region_name,
+            )
+        # AWS Profile based authentication
+        else:
+            aws_session = boto3.session.Session(profile_name=aws_profile)
+        if aws_endpoint_url:
+            client = aws_session.resource("dynamodb", endpoint_url=aws_endpoint_url)
+        else:
+            client = aws_session.resource("dynamodb")
+        self._client = client
+
+    def list_tables(self):
+        try:
+            tables = []
+            for table in self._client.tables.all():
+                tables.append(table)
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't list tables. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return tables
+
+    def get_items_iter(
+        self, table_name: str, scan_kwargs: dict = {"ConsistentRead": True}
+    ):
+        table = self._client.Table(table_name)
+        try:
+            done = False
+            start_key = None
+            while not done:
+                if start_key:
+                    scan_kwargs["ExclusiveStartKey"] = start_key
+                response = table.scan(**scan_kwargs)
+                yield response.get("Items", [])
+                start_key = response.get("LastEvaluatedKey", None)
+                done = start_key is None
+        except ClientError as err:
+            self.logger.error(
+                "Couldn't scan for movies. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
---
+++ tests/test_dynamodb.py
@@ -0,0 +1,78 @@
+import boto3
+from moto import mock_dynamodb
+
+from tap_dynamodb.dynamo import DynamoDB
+
+
+def create_table(moto_conn, name):
+    return moto_conn.create_table(
+        TableName=name,
+        KeySchema=[
+            {"AttributeName": "year", "KeyType": "HASH"},
+            {"AttributeName": "title", "KeyType": "RANGE"},
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "year", "AttributeType": "N"},
+            {"AttributeName": "title", "AttributeType": "S"},
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
+    )
+
+
+@mock_dynamodb
+def test_list_tables():
+    # PREP
+    moto_conn = boto3.resource("dynamodb")
+    for num in range(1, 106):
+        create_table(moto_conn, f"table_{num}")
+    # END PREP
+
+    db_obj = DynamoDB()
+    db_obj.authenticate({"aws_profile": "foo"})
+    tables = db_obj.list_tables()
+    assert len(tables) == 105
+    assert tables[0].table_name == "table_1"
+    assert tables[-1].table_name == "table_105"
+
+
+@mock_dynamodb
+def test_get_items():
+    # PREP
+    moto_conn = boto3.resource("dynamodb")
+    table = create_table(moto_conn, "table")
+    table.put_item(Item={"year": 2023, "title": "foo", "info": {"plot": "bar"}})
+    # END PREP
+
+    db_obj = DynamoDB()
+    db_obj.authenticate({"aws_profile": "foo"})
+    records = list(db_obj.get_items_iter("table"))[0]
+    assert len(records) == 1
+    assert records[0].get("year") == 2023
+    assert records[0].get("title") == "foo"
+    assert records[0].get("info") == {"plot": "bar"}
+
+
+@mock_dynamodb
+def test_get_items_paginate():
+    # PREP
+    moto_conn = boto3.resource("dynamodb")
+    table = create_table(moto_conn, "table")
+    for num in range(5):
+        table.put_item(
+            Item={"year": 2023, "title": f"foo_{num}", "info": {"plot": "bar"}}
+        )
+    # END PREP
+
+    db_obj = DynamoDB()
+    db_obj.authenticate({"aws_profile": "foo"})
+    iterations = 0
+    records = []
+    for i in db_obj.get_items_iter("table", {"Limit": 1, "ConsistentRead": True}):
+        iterations += 1
+        records.extend(i)
+    assert len(records) == 5
+    assert iterations == 5
+    first_item = records[0]
+    assert first_item.get("year") == 2023
+    assert first_item.get("title") == "foo_0"
+    assert first_item.get("info") == {"plot": "bar"}
